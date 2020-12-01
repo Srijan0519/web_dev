31781fe8478b1bfd6086bdb8f5bded7eca51eeb2
Khushi Arora <akhushi72@gmail.com>
2020-12-02 03:05:09 +0530

    task 1 js

--- khushi/JS Task1/index.js
+++ khushi/JS Task1/index.js
@@ -8,21 +8,21 @@ function getUserRole(namee, role)
 
     switch(role){
                     case "admin":
-                        alert("your name is"+namee"and you have all access");
+                        alert("your name is "+namee+"and you have all access");
                         break;
                     case "subadmin":
-                        alert("your name is"+namee"and you have access to create/delete courses");
+                        alert("your name is "+namee+"and you have access to create/delete courses");
                         break;
                     case "testprep":
-                        alert("your name is"+namee"and you have access to create/delete tests");
+                        alert("your name is "+namee+"and you have access to create/delete tests");
                         break;
                     case "user":
-                        alert("your name is"+namee"and you have access to all courses");
+                        alert("your name is "+namee+"and you have access to all courses");
                         break;
                     case "other":
-                        alert("your name is"+namee"and you have access to trial version");
+                        alert("your name is "+namee+"and you have access to trial version");
                     default:
-                        alert("your name is"+namee"and you have access to trial version");
+                        alert("your name is "+namee+"and you have access to trial version");
                         break;
 
 
