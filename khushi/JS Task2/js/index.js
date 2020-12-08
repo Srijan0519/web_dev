f57582a3b1640ad7ef48a031daa9ef906996985d
Khushi Arora <akhushi72@gmail.com>
2020-12-08 19:56:15 +0530

    task 2 js

---
+++ khushi/JS Task2/js/index.js
@@ -0,0 +1,12 @@
+
+var x= document.getElementById("num");
+ 
+ function isEven()
+ {
+     if(x % 2 == 0)
+     {console.log("the number is even");}
+
+     else {console.log("the number is not even");}
+ }
+
+ 
